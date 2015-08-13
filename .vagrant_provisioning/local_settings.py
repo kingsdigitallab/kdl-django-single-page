4ef70b49119e0115d4b08a70ec5d0d42fb1e3c5f
Miguel Vieira <jose.m.vieira@kcl.ac.uk>
2015-08-13 09:52:53 +0100

    Added default local settings.

---
+++ .vagrant_provisioning/local_settings.py
@@ -0,0 +1,16 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'kdl',
+        'USER': 'app_kdl',
+        'PASSWORD': 'kdl',
+        'ADMINUSER': 'postgres',
+        'HOST': 'localhost'
+    },
+}
+
+INTERNAL_IPS = ('0.0.0.0', '127.0.0.1', '::1')
+
+SECRET_KEY = '12345'
+
+FABRIC_USER = 'jvieira'
