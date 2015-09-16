236b5c2ca64809ba2e3ff9a390d8b690046fbf32
Miguel Vieira <jose.m.vieira@kcl.ac.uk>
2015-09-16 12:19:31 +0100

    Added new entry to allowed hosts.
    --HG--
    branch : liv

--- kdl/settings/liv.py
+++ kdl/settings/liv.py
@@ -4,7 +4,7 @@ CACHE_REDIS_DATABASE = '2'
 CACHES['default']['LOCATION'] = '127.0.0.1:6379:' + CACHE_REDIS_DATABASE
 
 INTERNAL_IPS = INTERNAL_IPS + ('137.73.254.10', )
-ALLOWED_HOSTS = ['kdl.kcl.ac.uk', 'kdl.cch.kcl.ac.uk']
+ALLOWED_HOSTS = ['kdl.kcl.ac.uk', 'www.kdl.kcl.ac.uk', 'kdl.cch.kcl.ac.uk']
 
 DATABASES = {
     'default': {
