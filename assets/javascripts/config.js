6e024ad777fdba053ec4aadd4a3bbca683fb1285
Ginestra Ferraro <ginestra.ferraro@kcl.ac.uk>
2015-09-30 16:52:56 +0100

    Initial redesign for one-page site

---
+++ assets/javascripts/config.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+
+    // jQuery for page scrolling feature - requires jQuery Easing plugin
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: ($($anchor.attr('href')).offset().top - 180)
+        }, 1250, 'easeInOutExpo');
+        event.preventDefault();
+    });
+
+    // Fit Text Plugin for Main Header
+    $("h1").fitText(
+        1.2, {
+            minFontSize: '35px',
+            maxFontSize: '65px'
+        }
+    );
+
+    // Initialize WOW.js Scrolling Animations
+    new WOW().init();
+
+});
